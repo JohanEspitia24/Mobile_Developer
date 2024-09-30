28ad205f39826939e2c8131b3d7587b67e8f5b0b
JohanEspitia24 <71570769+JohanEspitia24@users.noreply.github.com>
2024-09-30 17:48:46 -0500

    Update summary

--- app/src/main/java/com/example/taller_2/MainActivity.kt
+++ app/src/main/java/com/example/taller_2/MainActivity.kt
@@ -1,28 +1,28 @@
-package com.example.taller_2
+package com.example.taller2
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.taller_2.databinding.ActivityMainBinding
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
-        // Manejar el click para abrir cámara o galería
-        binding.btnCameraGallery.setOnClickListener {
-            val intent = Intent(this, CameraGalleryActivity::class.java)
+        val btnImage = findViewById<ImageView>(R.id.btnImage)
+        val btnMap = findViewById<ImageView>(R.id.btnMap)
+
+        // Lanzar la actividad de galería y cámara
+        btnImage.setOnClickListener {
+            val intent = Intent(this, ImageVideoActivity::class.java)
             startActivity(intent)
         }
 
-        // Manejar el click para abrir mapas
-        binding.btnMap.setOnClickListener {
-            val intent = Intent(this, MapsActivity::class.java)
+        // Lanzar la actividad de mapa
+        btnMap.setOnClickListener {
+            val intent = Intent(this, MapActivity::class.java)
             startActivity(intent)
         }
     }
---
+++ app/src/main/java/com/example/taller_2/MapActivity.kt
@@ -0,0 +1,4 @@
+package com.example.taller_2
+
+class MapActivity {
+}
